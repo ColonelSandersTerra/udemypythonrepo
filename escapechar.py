d79f116126af1031835711ebcfdf5d56780b68b4
Victor Yip <victorgyip@gmail.com>
2022-11-01 19:08:19 -0400

    add some files 1

---
+++ escapechar.py
@@ -0,0 +1,27 @@
+splitString = "This string has been \nsplit over\nseveral\nlines"
+print(splitString)
+tabbedstring = "1\t2\n3\t4"
+print(tabbedstring)
+
+print('The pet shop owner said "No, no, \'e\'s uh, ...he\'s resting".')
+# or
+print ("The pet show owner said \"No, no, 'e's uh, ...he's resting\".")
+
+
+print ("""The pet show owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplitString = """This string has been \
+    split over \
+    several \
+    lines """
+
+print(anotherSplitString)
+
+print(anotherSplitString)
+
+
+
+print(r"C:\Users\timbuchalka\notes.txt")
+print("C:\\Users\\timbuchalka\\notes.txt")
+
+
---
+++ strings.py
@@ -0,0 +1,26 @@
+print("hello" + "world")
+#name =  input("Please enter your name: ")
+#print(name)
+
+for i in range(1,5):
+    print(i)
+
+greeting = "Hello"
+name = "Tim"
+print (greeting + " " + name)
+
+age = 24
+print(type(greeting))
+
+
+
+
+
+age = 24
+print(type(age))
+print(age)
+print(type(age))
+
+age_in_words = "2 years"
+print (name + " is " + str(age) + " years old.")
+print(type(age))
