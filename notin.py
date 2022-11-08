2d2cf342f544114152b59a32eea1aa2b5c16622b
Victor Yip <victorgyip@gmail.com>
2022-11-08 00:20:42 -0500

    lesson 59

---
+++ notin.py
@@ -0,0 +1,5 @@
+activity = input("What would you like to do today? ")
+
+if "cinema" not in activity.casefold(): #handles lower case and other language letters converted to English equivalents
+    print("But I want to go to the cinema")
+
--- string2.py
+++ string2.py
@@ -1,29 +1,31 @@
 #
 #         012345678901234
-parrot = "Norwegian Blue"
+# parrot = "Norwegian Blue"
 
-print(parrot)
+# print(parrot)
 
-print(parrot[3])
+# print(parrot[3])
 
-print(parrot[0:6]) #Norweg
-print(parrot[:9])
-print(parrot[10:])
-#if no : in brackets, that is an index
-print()
-print(parrot[-4:-2])
-print(parrot[-4:12])
+# print(parrot[0:6]) #Norweg
+# print(parrot[:9])
+# print(parrot[10:])
+# #if no : in brackets, that is an index
+# print()
+# print(parrot[-4:-2])
+# print(parrot[-4:12])
 
-print(parrot[0:6:2])
-print(parrot[0:6:3])
+# print(parrot[0:6:2])
+# print(parrot[0:6:3])
 
-number = "9,233:372:036 854,775;807"
-print(number[1::4])
+number = input("Please enter a series of numbers, using any separators you like: ")
+separators = ""
 
-seperators = number[1::4]
-print(seperators)
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
 
-values = "".join(char if char not in seperators else " " for char in number).split()
-print([int(val) for val in values])
+print(separators)
 
+values = "".join(char if char not in separators else " " for char in number).split()
+print(sum([int(val) for val in values]))
 
